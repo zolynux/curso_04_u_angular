80b52797466b8eae382572b9aedc7e1ffb5fee2a
zolynux <152818486+zolynux@users.noreply.github.com>
2024-08-07 16:22:36 -0500

    Sección 2 -> elección 1 Creación de Componente Manualmente en Angular

---
+++ seccion_02_fundamentos_de_angular/src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { PersonasComponent } from './personas/personas.component';
+
+@NgModule({
+  declarations: [PersonasComponent],
+  exports: [PersonasComponent],
+  providers: [],
+})
+export class AppModule {}
