da1bc9b2f519a5f463c4437d2fd3c41ca7155808
zolynux <152818486+zolynux@users.noreply.github.com>
2024-08-07 17:04:42 -0500

    Sección 2 -> lección 5 Aplicando Estilos CSS a Angular

--- seccion_02_fundamentos_de_angular/src/app/personas/personas.component.ts
+++ seccion_02_fundamentos_de_angular/src/app/personas/personas.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-personas',
-  template: `
-    <h1 class="text-4xl">Listado de Personas</h1>
-    <app-persona></app-persona>
-    <app-persona></app-persona>
-  `,
+  templateUrl: './personas.component.html',
+  styleUrls: ['./personas.component.css'],
 })
 export class PersonasComponent {}
