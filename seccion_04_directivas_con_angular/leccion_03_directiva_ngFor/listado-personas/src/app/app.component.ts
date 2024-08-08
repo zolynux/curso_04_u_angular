52db0ad7730b941a79b29862542b4bd480f6d92d
zolynux <152818486+zolynux@users.noreply.github.com>
2024-08-08 13:16:31 -0500

    Sección 4 Directivas con Angular: lección 3.2 Uso directiva ngFor

--- seccion_04_directivas_con_angular/leccion_03_directiva_ngFor/listado-personas/src/app/app.component.ts
+++ seccion_04_directivas_con_angular/leccion_03_directiva_ngFor/listado-personas/src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AppModule } from './app.module';
+import { Persona } from './persona.model';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppModule],
+  imports: [FormsModule, CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   titulo = 'Listado de Personas';
+  personas: Persona[] = [
+    new Persona('Juan', 'Perez'),
+    new Persona('Laura', 'Lopez'),
+    new Persona('Carlo', 'Ramirez'),
+  ];
 }
