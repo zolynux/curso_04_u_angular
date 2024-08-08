27c739d36ba522678035817170aa3c856af0da2f
zolynux <152818486+zolynux@users.noreply.github.com>
2024-08-08 12:58:49 -0500

    Sección 4 Directivas con Angular: lección 3.1 Creación Clase Persona

--- seccion_04_directivas_con_angular/leccion_03_directiva_ngFor/listado-personas/src/app/app.component.ts
+++ seccion_04_directivas_con_angular/leccion_03_directiva_ngFor/listado-personas/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppModule } from './app.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'listado-personas';
+  titulo = 'Listado de Personas';
 }
