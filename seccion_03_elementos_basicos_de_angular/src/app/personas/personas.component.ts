6b2fe709e89d09f91c6eff7134f333f5bb316ad2
zolynux <152818486+zolynux@users.noreply.github.com>
2024-08-07 19:48:59 -0500

    Sección 3 Elementos Básicos De Angular: lección 4 Event Binding en Angular

--- seccion_03_elementos_basicos_de_angular/src/app/personas/personas.component.ts
+++ seccion_03_elementos_basicos_de_angular/src/app/personas/personas.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class PersonasComponent {
   deshabilitar = false;
+  mensaje = 'No se ha agregado ninguna persona';
+  titulo = '';
+
+  agregarPersona() {
+    this.mensaje = 'Persona agregada';
+  }
+
+  modificarTitulo(event: Event) {
+    console.log('Entrando a método modificar título');
+    this.titulo = (<HTMLInputElement>event.target).value;
+  }
 }
